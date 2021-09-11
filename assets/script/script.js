71d56d324b501b34d58fa46980ab06a1fe086d3a
curriecoder <andrew.j.currie0@gmail.com>
2021-09-11 14:19:04 -0400

    connected style.css and script.j to index.html

---
+++ assets/script/script.js
@@ -0,0 +1,16 @@
+/* WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and my score --> */
+
+var body = document.body;
+var h1 = document.createElement("h1");
+
+h1.textContent = "Testing";
+body.appendChild(h1);
