a9610ccb8a67259e7cc0fb2777a9c908c125811c
curriecoder <andrew.j.currie0@gmail.com>
2021-09-11 16:33:48 -0400

    added script to have start page move on to question 1 on bu
    ls
    cd
    
    s
    ls

--- assets/script/script.js
+++ assets/script/script.js
@@ -22,3 +22,18 @@ THEN I can save my initials and my score --> */
 // h1.setAttribute("style", "color: white");
 
 // body.appendChild(h1);
+
+var body = document.body;
+var cardContents = document.querySelector("#card-contents")
+var startBtn = document.querySelector("#start");
+var h4Tags = document.querySelector("#title");
+var text = document.querySelector("#question-text");
+
+startBtn.addEventListener("click", function() {
+  h4Tags.textContent = "Question 1";
+  cardContents.appendChild(h4Tags);
+  text.textContent = "Is this really a test question?";
+  cardContents.appendChild(text);
+  startBtn.setAttribute("style", "display: none");
+});
+
