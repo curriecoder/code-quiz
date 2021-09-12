b1e55cb68c3c85079ce394babd3a15b9ccd6c2f3
curriecoder <andrew.j.currie0@gmail.com>
2021-09-12 11:51:24 -0400

    added countdown-timer when quiz begins

--- assets/script/script.js
+++ assets/script/script.js
@@ -14,26 +14,48 @@ WHEN the game is over
 THEN I can save my initials and my score --> */
 
 
-// var body = document.body;
-// var h1 = document.createElement("h1");
-
-// h1.textContent = "Testing";
-
-// h1.setAttribute("style", "color: white");
-
-// body.appendChild(h1);
-
 var body = document.body;
 var cardContents = document.querySelector("#card-contents")
 var startBtn = document.querySelector("#start");
 var h4Tags = document.querySelector("#title");
-var text = document.querySelector("#question-text");
+var questionText = document.querySelector("#question-text");
+
 
 startBtn.addEventListener("click", function() {
   h4Tags.textContent = "Question 1";
   cardContents.appendChild(h4Tags);
-  text.textContent = "Is this really a test question?";
-  cardContents.appendChild(text);
+  questionText.textContent = "Is this really a test question?";
+  cardContents.appendChild(questionText);
   startBtn.setAttribute("style", "display: none");
+  countdown();
 });
 
+var timeLeft = 60;
+var timer = document.createElement("p")
+
+function countdown() {
+  var timeInterval = setInterval(function() {
+        
+    timer.textContent = timeLeft;
+  
+    timer.setAttribute("style", "position: absolute; top: 25px; right: 25px")
+  
+    body.appendChild(timer);
+
+    if (timeLeft > 1) {
+      timer.textContent = "Time remaining: " + timeLeft + " seconds";
+      timeLeft--;
+    } else if (timeLeft === 1) {
+      timer.textContent = "Time remaining: " + timeLeft + " second";
+      timeLeft--;
+    } else {
+      timer.textContent = "";
+      clearInterval(timeInterval);
+      displayMessage();
+    }
+  }, 1000);
+}
+
+function displayMessage() {
+  cardContents.textContent = "Time's up!"
+}
