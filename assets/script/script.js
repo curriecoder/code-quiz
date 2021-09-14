d82ea8ab39e8af84ddb0607e42151d83907a9097
curriecoder <andrew.j.currie0@gmail.com>
2021-09-14 18:48:19 -0400

    began adding more functions to improve control of data

--- assets/script/script.js
+++ assets/script/script.js
@@ -17,38 +17,86 @@ THEN I can save my initials and my score --> */
 var body = document.body;
 var cardContents = document.querySelector("#card-contents")
 var startBtn = document.querySelector("#start");
-var h4Tags = document.querySelector("#title");
+var questionTitle = document.querySelector("#title");
 var questionText = document.querySelector("#question-text");
 var answerList = document.createElement("ul");
-// create buttons for answers
-var answerBtnA = document.createElement("button");
-var answerBtnB = document.createElement("button");
-var answerBtnC = document.createElement("button");
-var answerBtnD = document.createElement("button");
+
 // add text for answers
 var answerTextA = document.createElement("li");
 var answerTextB = document.createElement("li");
 var answerTextC = document.createElement("li");
 var answerTextD = document.createElement("li");
+var currentQuestion = 0;
+
+//make the questions objects so we can change the text for the question and the answers (.value for content of key)
+//make an array for all of our questions so we can iterate when the page loads next question
+var questions = [
+  {questionNumber: "Question 1",
+  questionText: "The condition in an if/else statement is enclosed within _____. ",
+  answerList: ["Quotes", "Curly Brackets", "Parenthesis", "Square Brackets"],
+  correctAnswer: "Curly Brackets"},
+  {questionNumber: "Question 2",
+  questionText: "Arrays in JavaScript can be used to store _____.",
+  answerList: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the Above"],
+  correctAnswer: "All of the Above"},
+  {questionNumber: "Question 3",
+  questionText: "String values must be enclosed within _____ when being assigned to variables.",
+  answerList: ["Commas", "Curly Brackets", "Quotes", "Parenthesis"],
+  correctAnswer: "Quotes"},
+  {questionNumber: "Question 4",
+  questionText: "A very useful tool used during development and debugging for printing content to the debugger is:",
+  answerList: ["JavaScript", "Terminal/Bash", "For Loops", "Console.log"],
+  correctAnswer: "Console.log"},
+  {questionNumber: "Question 5",
+  questionText: "Commonly used data types DO NOT include:",
+  answerList: ["Strings", "Booleans", "Alerts", "Numbers"],
+  correctAnswer: "Alerts"}
+];
+
+
+answerList.addEventListener("click", function(event) {
+  var element = event.target;
+  if (element.matches("button")) {
+    if (currentQuestion < questions.length) {
+      currentQuestion++;
+      console.log(currentQuestion);
+    } else {
+      alert("end of quiz");
+    }
+  }
+});
 
 // creates start of quiz on startBtn 'click'
-startBtn.addEventListener("click", function() {
-  countdown();
-  h4Tags.textContent = "Question 1";
-  questionText.textContent = "Is this really a test question?";
-  answerTextA.textContent = "This is answer A";
-  answerTextB.textContent = "This is answer B";
-  answerTextC.textContent = "This is answer C";
-  answerTextD.textContent = "This is answer D";
+// when the quiz starts, have  the first question (questions[0]) display on the screen
+//when user clicks an answer btn, the question changes to the next question in the array (++)
+//if the answer is incorrect, subtract 10 seconds from the countdown.
 
+startBtn.addEventListener("click", startQuiz);
+
+function startQuiz(event) {
+  event.preventDefault();
+  countdown();
   startBtn.setAttribute("style", "display: none");
-  answerList.setAttribute("style", "display: flex; flex-direction: column; align-items: center");
-  answerTextA.setAttribute("style", "display: flex; flex-direction: row-reverse; align-items: center")
-  answerTextB.setAttribute("style", "display: flex; flex-direction: row-reverse; align-items: center")
-  answerTextC.setAttribute("style", "display: flex; flex-direction: row-reverse; align-items: center")
-  answerTextD.setAttribute("style", "display: flex; flex-direction: row-reverse; align-items: center")
   
-  cardContents.appendChild(h4Tags);
+  // create buttons for answers
+  var answerBtnA = document.createElement("button");
+  answerBtnA.textContent = "A";
+  var answerBtnB = document.createElement("button");
+  answerBtnB.textContent = "B";
+  var answerBtnC = document.createElement("button");
+  answerBtnC.textContent = "C";
+  var answerBtnD = document.createElement("button");
+  answerBtnD.textContent = "D";
+  
+  //we want these question1's to be question[i] so we can change through the questions
+  questionTitle.textContent = questions[currentQuestion].questionNumber;
+  questionText.textContent = questions[currentQuestion].questionText;
+  answerTextA.textContent = questions[currentQuestion].answerList[0];
+  answerTextB.textContent = questions[currentQuestion].answerList[1];
+  answerTextC.textContent = questions[currentQuestion].answerList[2];
+  answerTextD.textContent = questions[currentQuestion].answerList[3];
+ 
+  cardContents.appendChild(questionTitle);
   cardContents.appendChild(questionText);
   cardContents.appendChild(answerList);
   answerTextA.appendChild(answerBtnA);
@@ -59,44 +107,21 @@ startBtn.addEventListener("click", function() {
   answerList.appendChild(answerTextC);
   answerTextD.appendChild(answerBtnD);
   answerList.appendChild(answerTextD);
-});
+  
+};
 
-//make an array for all of our questions so we can iterate when the page loads next question
-//make the questions objescts so we can change the text for the question and the answers (.value for content of key)
-//when user clicks an answer btn, the question changes to the next question in the array (++)
-//if the answer is incorrect, subtract 10 seconds from the countdown.
 
-var questions = [question1, question2, question3, question4, question5];
-
-var question1 = {
-  questionText: "The condition in an if/else statement is enclosed within _____. ",
-  answerList: ["Quotes", "Curly Brackets", "Parenthesis", "Square Brackets"],
-  correctAnswer: "Curly Brackets"
-}
-
-var question2 = {
-  questionText: "Arrays in JavaScript can be used to store _____.",
-  answerList: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the Above"],
-  correctAnswer: "All of the Above"
-}
-
-var question3 = {
-  questionText: "String values must be enclosed within _____ when being assigned to variables.",
-  answerList: ["Commas", "Curly Brackets", "Quotes", "Parenthesis"],
-  correctAnswer: "Quotes"
-}
-
-var question4 = {
-  questionText: "A very useful tool used during development and debugging for printing content to the debugger is:",
-  answerList: ["JavaScript", "Terminal/Bash", "For Loops", "Console.log"],
-  correctAnswer: "Console.log"
-}
-
-var question5 = {
-  questionText: "Commonly used data types DO NOT include:",
-  answerList: ["Strings", "Booleans", "Alerts", "Numbers"],
-  correctAnswer: "Alerts"
-}
+function nextQuestion() {
+  // If there are no more questions, ends the quiz
+  if (currentQuestion === questions.length || endQuiz) {
+      if (timeLeft > 0) {
+          score = timeLeft;
+      } else {
+          score = 0;
+      }
+      quizOver();
+      return;
+  }
 
 
 // creates timer and counts down from 60 seconds 
@@ -105,13 +130,13 @@ var timer = document.createElement("p")
 
 function countdown() {
   var timeInterval = setInterval(function() {
-        
+    
     timer.textContent = timeLeft;
-  
+    
     timer.setAttribute("style", "position: absolute; top: 25px; right: 30px")
-  
+    
     body.appendChild(timer);
-
+    
     if (timeLeft > 1) {
       timer.textContent = "Time remaining: " + timeLeft + " seconds";
       timeLeft--;
@@ -122,9 +147,11 @@ function countdown() {
       timer.textContent = "";
       clearInterval(timeInterval);
       displayMessage();
-    }
-  }, 1000);
-}
+    }  
+  }, 1000);  
+}  
+
+
 
 // creates end of game message
 function displayMessage() {
